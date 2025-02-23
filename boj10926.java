cc2aa32df679459b71691b31674a60838439338b
wisdom0405 <wisdom0405@kakao.com>
2025-02-23 19:48:18 +0900

    boj0223

--- boj10926.java
+++ boj10926.java
@@ -1,2 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class boj10926 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine() + "??!");
+    }
 }
