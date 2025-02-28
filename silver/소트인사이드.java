e6783af24fd6530edf727525ac2f91ea705e74bd
wisdom0405 <wisdom0405@kakao.com>
2025-02-28 14:05:56 +0900

    boj0227

---
+++ silver/소트인사이드.java
@@ -0,0 +1,16 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 소트인사이드 {
+    public static void main (String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+        int[] A  = new int[N.length()];
+
+
+    }
+}
