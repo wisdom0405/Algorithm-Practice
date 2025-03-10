6d702b6fff56a0b523a4a12b26e92086d8758f2d
wisdom0405 <wisdom0405@kakao.com>
2025-03-10 21:23:20 +0900

    boj0310

---
+++ bronze/최소공배수.java
@@ -0,0 +1,30 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 최소공배수 {
+    static int A;
+    static int B;
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine()); // 질의 개수
+
+        for(int i=0; i<T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            int gcd = GCD(A,B);
+            int result = A*B/gcd;
+            System.out.println(result);
+        }
+    }
+    private static int GCD(int A, int B) {
+        if (B == 0) return A;
+        else {
+            return GCD(B, A % B);
+        }
+    }
+}
---
+++ gold/gcd.java
@@ -0,0 +1,23 @@
+package gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class gcd {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        long N = Long.parseLong(br.readLine());
+//
+//        // 배열에 값 채우기
+//        long A[] = new long[N+1];
+//
+//        for(long i=2; i<A.length; i++){ // 1은 소수 아니므로 2부터 시작
+//            A[i] = i;
+//        }
+//
+//        // 오일러피 구현 + 소수찾기 (에라토스테네스의 체)
+//        for(int i=2;)
+//
+//    }
+}
